e08f603b364f1ba831ccbf86740a243c5ade943b
Vadin0 <vaidik2007.16@gmail.com>
2025-11-23 15:10:17 +0530

    .

--- arrays/reversing_array.cpp
+++ arrays/reversing_array.cpp
@@ -1,3 +1,22 @@
-#include<iosteam>
+#include<iostream>
+#include<vector>
 using namespace std;
 
+void reversearray(vector<int>& arr){
+    int st=0;
+    int end=arr.size()-1;
+      for(int i=0;i<end;i++){
+    swap(arr[st],arr[end]);
+    st++;end--;
+ }   
+}
+
+int main(){
+    vector<int> arr1={1,2,3,4,5,6,7,8,9};
+    cout<<"your reversed array is"<<endl;
+    reversearray(arr1);
+    
+   for(int i=0;i<arr1.size();i++){
+    cout<<arr1[i]<<" ";
+   }
+}
